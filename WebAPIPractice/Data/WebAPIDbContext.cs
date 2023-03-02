c293e3fad877c4d1d4898941bccae5a123839d63
Pattymania <tandi.sumit309@gmail.com>
2023-03-02 15:41:35 +0530

    Checkin changes

---
+++ WebAPIPractice/Data/WebAPIDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPIPractice.Models.Domain;
+
+namespace WebAPIPractice.Data
+{
+    public class WebAPIDbContext:DbContext
+    {
+        public WebAPIDbContext(DbContextOptions<WebAPIDbContext> options): base(options)
+        {
+            
+        }
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
